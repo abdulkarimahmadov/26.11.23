135496f9bf21fc64c9b20fd47aaff76db3378322
Abdulkarim Ahmadov <148267382+abdulkarimahmadov@users.noreply.github.com>
2023-11-26 09:41:35 +0400

    new

---
+++ ConsoleApp/CustomGenList.cs
@@ -0,0 +1,68 @@
+﻿namespace MyCustomNamespace
+{
+    public class CustomGenList<T>
+    {
+        private List<T> items;
+        private int capacity;
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public CustomGenList()
+        {
+            items = new List<T>();
+            capacity = 0;
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+
+            if (items.Count >= capacity)
+            {
+                if (capacity == 0)
+                {
+                    capacity = 4;
+                }
+                else
+                {
+                    capacity *= 2;
+                }
+            }
+            count++;
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            return items.Find(match);
+        }
+
+        public List<T> FindAll(Predicate<T> match)
+        {
+            return items.FindAll(match);
+        }
+
+        public bool Contains(T item)
+        {
+            return items.Contains(item);
+        }
+
+        public bool Exists(Predicate<T> match)
+        {
+            return items.Exists(match);
+        }
+
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using MyCustomNamespace;
+
+CustomGenList<int> customGenList = new();
+
+customGenList.Add(1);
+customGenList.Add(2);
+customGenList.Add(3);
+customGenList.Add(4);
+customGenList.Add(5);
+customGenList.Add(6);
+customGenList.Add(7);
+customGenList.Add(8);
+customGenList.Add(9);
+customGenList.Add(10);
+
+Console.WriteLine(customGenList.Count);
+Console.WriteLine(customGenList.Capacity);
